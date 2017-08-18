458551fa94e19b5ae206051e92ffb5178f1e1958
IdealSolution <altin.oga@ideal.al>
2017-08-18 15:21:26 +0200

    error

--- WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ShowDialog();
+            this.ShowDialog();
         }
     }
 }
